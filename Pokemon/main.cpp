91fb7237ebd56f5b0eccb8551da9f6edc7f5edb5
Mayur Mansing Nangare <163443687+nangareSama@users.noreply.github.com>
2025-05-12 10:30:05 +0530

    switch statement exercise

--- Pokemon/main.cpp
+++ Pokemon/main.cpp
@@ -1,39 +1,42 @@
 #include <iostream>
 using namespace std;
 
-
-
 int main() {
-   cout << "Please enter the Pokemon you Choose \n there are 3 choices :\n 1.Bulbasaur \n 2.Charmander \n 3.Squirtle \n";
-  int playerinput;
-   cin >> playerinput;
+    int choice;
+    string initialNarration = R"(Welcome, brave adventurer!
+    You find yourself standing in front of a massive, ancient castle.
+    The walls are cracked with age,
+    and the doors creak as they slowly open in front of you.
+    Inside, there are many rooms, each more mysterious than the last.
+    Some may hold treasures beyond imagination,
+    while others may hide dangers lurking in the shadows.
+    )";
+    
+    string missionObjective = R"(Mission Objective:
+    Your goal is simple, yet perilous—choose a room!
+    Once inside, who knows what you may find?
+    Will it be riches, knowledge, or something… more sinister?
+    It’s all up to the room you pick.
+    )";
 
+    cout << initialNarration;
+    cout << missionObjective;
 
-//    if(playerinput == 1){
-//     cout << "You chose Bulbasaur! A wise choice.\n";
-//    }else if(playerinput == 2){
-//     cout << "You chose Charmander! A fiery choice.\n";
-//    }else if(playerinput == 3){
-//     cout << "You chose Squirtle! A cool choice.\n";
-//    }else {
-//     cout << "Pikachu !\n";
-//    }
+    cout << "Please choose your room by entering (1, 2, 3):\n";
+    cin >> choice;
 
-    switch(playerinput){
-        case 1 : 
-            cout << "You chose Bulbasaur! A wise choice.\n";
-        break;
-
-        case 2 : 
-            cout << "You chose Charmander! A fiery choice.\n";
-        break;
-
-        case 3 : 
-            cout << "You chose Squirtle! A cool choice.\n";
-        break;
-
-        default : 
-        cout << "Invalid choice.\n";
+    switch(choice) {
+        case 1:
+            cout << "A room full of gold coins! You can retire now, you lucky soul!\n";
+            break;
+        case 2:
+            cout << "A library of ancient books! You gain immense knowledge, but beware… some knowledge comes with a price.\n";
+            break;
+        case 3:
+            cout << "A sleeping dragon! You barely escape with your life. Perhaps next time, choose more wisely...\n";
+            break;
+        default:
+            cout << "A secret passage to the dungeon! Beware, danger lies ahead in the dark corridors.\n";
     }
 
     return 0;
