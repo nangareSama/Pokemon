6cef2e18edd439a59790aefd9cd0796a2a1c9419
Mayur Mansing Nangare <163443687+nangareSama@users.noreply.github.com>
2025-05-15 14:59:20 +0530

    fixed dailog

--- Pokemon/scroll.cpp
+++ Pokemon/scroll.cpp
@@ -1,8 +1,18 @@
 #include<iostream>
 using namespace std;
 
-
+void waitForEnter(){
+    cin.get();
+}
 
 int main(){
+    cout << "Hello , I am Professor Oak." << endl;
+    waitForEnter();
+    cout << "I want to take rest." << endl;
+    waitForEnter();
+    cout << "I have to Enjoy this winter." << endl;
+    waitForEnter()
+    cout << "I want to eat Pizza." << endl;
 
+ return 0;
 }
